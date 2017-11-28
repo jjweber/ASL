3b50c8bbd905a6022d61ed0ac6106c07d34d0857
Your Name <you@example.com>
2017-11-28 01:07:29 -0500

    Added if statements for active nav class. Routing working. Bootstrap working correctly

--- src/AppBundle/Controller/WelcomeController.php
+++ src/AppBundle/Controller/WelcomeController.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jweber0169
- * Date: 11/27/17
- * Time: 3:34 PM
- */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class WelcomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+
+    public function indexAction() {
+        return $this->render('templates/index.html.twig', array( "name" => "home" ));
+    }
+
+    /**
+     * @Route("/hello", name="hellopage")
+     */
+
+    public function helloAction() {
+        return $this->render('templates/hello.html.twig', array( "name" => "hello" ));
+    }
+
+}
